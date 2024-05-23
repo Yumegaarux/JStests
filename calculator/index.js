6812abe4dcf5e4aa3f943eec20bc08fd0b0a65c3
Yumegaarux <zaldarriagajanvictor@gmail.com>
2024-05-23 22:55:30 +0800

    transferred onclick to html and changed the css a lot.

--- calculator/index.js
+++ calculator/index.js
@@ -1,18 +1,10 @@
-const btn1 = document.getElementById("btn1");
-const btn2 = document.getElementById("btn2");
-const btn3 = document.getElementById("btn3");
-const btn4 = document.getElementById("btn4");
-const btn5 = document.getElementById("btn5");
-const btn6 = document.getElementById("btn6");
-const btn7 = document.getElementById("btn7");
-const btn8 = document.getElementById("btn8");
-const btn9 = document.getElementById("btn9");
 const screenNum = document.getElementById("screen");
 
-btn1.onclick = function(){
-    inputCalculator("1");
-}
-
+    
 function inputCalculator(selectedNum){
     screenNum.value += selectedNum;
+}
+
+function inputDelete(){
+    screenNum.value--;
 }
