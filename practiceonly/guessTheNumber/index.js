ea777d38c8e9e28e33ab1f6cce002d1e1fd41e14
Yumegaarux <zaldarriagajanvictor@gmail.com>
2024-06-05 20:19:23 +0800

    guessing game now working with score

--- practiceonly/guessTheNumber/index.js
+++ practiceonly/guessTheNumber/index.js
@@ -1,3 +1,17 @@
 let randomNum = Math.floor(Math.random() * 11);
+let score = 5;
 
-console.log(randomNum);
+function guess(){
+    let guessNumber = document.getElementById("guessNumber").value;
+    if (guessNumber == randomNum){
+        document.getElementById("numberHolder").textContent = "You win! Your score is: " + score;
+    }
+    else if(guessNumber < randomNum){
+        document.getElementById("numberHolder").textContent = "Your number is a bit too low, try again!";
+        score--;
+    }
+    else if(guessNumber > randomNum){
+        document.getElementById("numberHolder").textContent = "Your number is a bit too high, try again!";
+        score--;
+    }
+}
